bb46a683c7826a3fb4eb95aa152f7b12eec7b968
Clarence Manuel <rencedm112@gmail.com>
2025-07-24 20:25:14 +0800

    feat: use bytes crate for bridge_rpc

--- crates/bridge_rpc/src/transport.rs
+++ crates/bridge_rpc/src/transport.rs
@@ -1,10 +1,12 @@
 use std::fmt::Display;
 
+use bytes::Bytes;
+
 #[cfg_attr(test, mockall::automock(type Error = String;))]
 #[async_trait::async_trait]
 pub trait Transport {
     type Error: Display;
 
-    async fn send(&self, data: Vec<u8>) -> Result<(), Self::Error>;
-    async fn receive(&self) -> Result<Vec<u8>, Self::Error>;
+    async fn send(&self, data: Bytes) -> Result<(), Self::Error>;
+    async fn receive(&self) -> Result<Bytes, Self::Error>;
 }
