13734da54a8df67b0976819415779be1898ba039
Clarence Manuel <rencedm112@gmail.com>
2025-08-12 10:59:56 +0800

    fix: exec command

--- cli/omni/src/commands/utils.rs
+++ cli/omni/src/commands/utils.rs
@@ -1,12 +1,19 @@
 pub fn report_execution_results(
     results: &[crate::executor::TaskExecutionResult],
 ) {
-    let skipped = results.iter().filter(|r| r.is_skipped()).count();
-    let completed = results.iter().filter(|r| r.is_completed()).count();
-    let errored = results
-        .iter()
-        .filter(|r| r.is_error_before_complete())
-        .count();
+    let mut skipped = 0;
+    let mut errored = 0;
+    let mut completed = 0;
+
+    for res in results {
+        if res.is_skipped() {
+            skipped += 1;
+        } else if res.is_error_before_complete() || !res.success() {
+            errored += 1;
+        } else if res.is_completed() && res.success() {
+            completed += 1;
+        }
+    }
 
     if completed > 0 {
         trace::info!("Completed: {completed}");
