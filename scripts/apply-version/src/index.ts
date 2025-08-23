092372c117519ae5887522fa5e251c1b3dfff759
Clarence Manuel <rencedm112@gmail.com>
2025-08-23 19:04:42 +0800

    feat: add apply-version script

---
+++ scripts/apply-version/src/index.ts
@@ -0,0 +1,46 @@
+import walk from "ignore-walk";
+
+const files = await walk({
+    ignoreFiles: [".omniignore", ".gitignore", ".npmignore", ".dockerignore"],
+    includeEmpty: false,
+    path: process.cwd(),
+    follow: false,
+});
+
+const FILES_TO_UPDATE = ["package.json", "Cargo.toml"];
+const REPLACEMENT_PATTERN = /0.0.0-semantically-released/g;
+
+let currentDir = process.cwd();
+
+while (!(await Bun.file(`${currentDir}/.version`).exists())) {
+    if (currentDir === "/") {
+        console.error("Could not find .version file");
+        process.exit(1);
+    }
+
+    currentDir = currentDir.replace(/\/[^/]+$/, "");
+}
+
+const versionText = await Bun.file(`${currentDir}/.version`).text();
+
+const VERSION_REGEX =
+    /^(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)(?:-((?:0|[1-9][0-9]*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9][0-9]*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/;
+
+if (!VERSION_REGEX.test(versionText)) {
+    console.error(`Invalid version format: ${versionText}`);
+    process.exit(1);
+}
+
+for (const file of files) {
+    for (const fileToUpdate of FILES_TO_UPDATE) {
+        if (file.endsWith(fileToUpdate)) {
+            console.log(`Apply version in ${file}`);
+            const content = await Bun.file(file).text();
+            const updatedContent = content.replace(
+                REPLACEMENT_PATTERN,
+                versionText,
+            );
+            await Bun.file(file).write(updatedContent);
+        }
+    }
+}
