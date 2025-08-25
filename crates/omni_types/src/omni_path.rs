381c3618937d6b1f3a05efeffe43cf4746b3a319
Clarence Manuel <rencedm112@gmail.com>
2025-08-25 17:46:20 +0800

    chore: add windows case on omni_types OmniPath test cases

--- crates/omni_types/src/omni_path.rs
+++ crates/omni_types/src/omni_path.rs
@@ -257,28 +257,53 @@ mod tests {
     fn test_resolve() {
         let path = OmniPath::new_ws_rooted("foo");
 
-        let base = enum_map::enum_map! {
-            Root::Workspace => Path::new("/workspace"),
-            Root::Project => Path::new("/project"),
+        let base = if cfg!(windows) {
+            enum_map::enum_map! {
+                Root::Workspace => Path::new("E:\\workspace"),
+                Root::Project => Path::new("E:\\project"),
+            }
+        } else {
+            enum_map::enum_map! {
+                Root::Workspace => Path::new("/workspace"),
+                Root::Project => Path::new("/project"),
+            }
         };
 
-        assert_eq!(path.resolve(&base), Path::new("/workspace/foo"));
+        if cfg!(windows) {
+            assert_eq!(path.resolve(&base), Path::new("E:\\workspace\\foo"));
+        } else {
+            assert_eq!(path.resolve(&base), Path::new("/workspace/foo"));
+        }
     }
 
     #[test]
     fn test_resolve_in_place() {
         let mut path = OmniPath::new_ws_rooted("foo");
 
-        let base = enum_map::enum_map! {
-            Root::Workspace => Path::new("/workspace"),
-            Root::Project => Path::new("/project"),
+        let base = if cfg!(windows) {
+            enum_map::enum_map! {
+                Root::Workspace => Path::new("E:\\workspace"),
+                Root::Project => Path::new("E:\\project"),
+            }
+        } else {
+            enum_map::enum_map! {
+                Root::Workspace => Path::new("/workspace"),
+                Root::Project => Path::new("/project"),
+            }
         };
 
         path.resolve_in_place(&base);
 
-        assert_eq!(
-            path.path().expect("path should be resolved"),
-            Path::new("/workspace/foo")
-        );
+        if cfg!(windows) {
+            assert_eq!(
+                path.path().expect("path should be resolved"),
+                Path::new("E:\\workspace\\foo")
+            );
+        } else {
+            assert_eq!(
+                path.path().expect("path should be resolved"),
+                Path::new("/workspace/foo")
+            );
+        }
     }
 }
