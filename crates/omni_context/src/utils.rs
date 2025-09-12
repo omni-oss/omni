4de4bbdfbcb77d09964bb2676b9e78ec4539ec29
Clarence Manuel <rencedm112@gmail.com>
2025-09-12 18:08:20 +0800

    feat: allow env:vars in workspace configuration

--- crates/omni_context/src/utils.rs
+++ crates/omni_context/src/utils.rs
@@ -3,10 +3,9 @@ use std::{collections::HashMap, ffi::OsString};
 use maps::Map;
 
 pub type EnvVarsMap = Map<String, String>;
+pub type EnvVarsOsMap = HashMap<OsString, OsString>;
 
-pub(crate) fn vars_os(
-    vars: &Map<String, String>,
-) -> HashMap<OsString, OsString> {
+pub(crate) fn vars_os(vars: &EnvVarsMap) -> EnvVarsOsMap {
     vars.iter()
         .map(|(k, v)| (k.clone().into(), v.clone().into()))
         .collect()
