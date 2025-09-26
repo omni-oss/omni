1996de0efbf3c32c0edd33240b81f886294d7a93
Clarence Manuel <rencedm112@gmail.com>
2025-09-26 18:26:26 +0800

    fix: path for omni file trace logs is relative to the current dir, should be relative to workspace root dir

--- cli/omni/src/lib.rs
+++ cli/omni/src/lib.rs
@@ -1,11 +1,14 @@
 #![feature(try_blocks)]
 
-use std::{path::PathBuf, process::ExitCode};
+use std::{
+    path::{Path, PathBuf},
+    process::ExitCode,
+};
 
 use clap::Parser as _;
 use omni_cli_core::{
     commands::{self, Cli, CliArgs, CliSubcommands},
-    context::{self, Context, ContextError},
+    context::{self, Context, ContextError, get_root_dir},
 };
 use omni_tracing_subscriber::TracingConfig;
 use system_traits::impls::RealSys;
@@ -35,15 +38,21 @@ fn exit(code: ExitCode) -> ! {
 fn ctx(
     args: &CliArgs,
     tracing: &TracingConfig,
+    ws_root_dir: Option<&Path>,
 ) -> Result<Context<RealSys>, ContextError> {
     let sys = RealSys;
-    context::from_args_and_sys(args, sys, tracing)
+    if let Some(root) = ws_root_dir {
+        context::from_args_root_dir_and_sys(args, root, sys, tracing)
+    } else {
+        context::from_args_and_sys(args, sys, tracing)
+    }
 }
 
 pub async fn run(
     sc: &CliSubcommands,
     args: &CliArgs,
     tracing: &TracingConfig,
+    ws_root_dir: Option<&Path>,
 ) -> eyre::Result<()> {
     match sc {
         CliSubcommands::Config(config) => {
@@ -53,28 +62,28 @@ pub async fn run(
             commands::completion::run(completion).await?;
         }
         CliSubcommands::Exec(exec) => {
-            let context = ctx(args, tracing)?;
+            let context = ctx(args, tracing, ws_root_dir)?;
             let res = commands::exec::run(exec, &context).await?;
             exit(res);
         }
         CliSubcommands::Env(env) => {
-            let mut context = ctx(args, tracing)?;
+            let mut context = ctx(args, tracing, ws_root_dir)?;
             commands::env::run(env, &mut context).await?;
         }
         CliSubcommands::Run(run) => {
-            let context = ctx(args, tracing)?;
+            let context = ctx(args, tracing, ws_root_dir)?;
             let res = commands::run::run(run, &context).await?;
             exit(res);
         }
         CliSubcommands::Hash(hash_command) => {
-            let context = ctx(args, tracing)?;
+            let context = ctx(args, tracing, ws_root_dir)?;
             commands::hash::run(hash_command, &context).await?;
         }
         CliSubcommands::Declspec(declspec_command) => {
             commands::declspec::run(declspec_command).await?;
         }
         CliSubcommands::Cache(cache_command) => {
-            let context = ctx(args, tracing)?;
+            let context = ctx(args, tracing, ws_root_dir)?;
             commands::cache::run(cache_command, &context).await?;
         }
     }
@@ -87,13 +96,30 @@ pub async fn main() -> eyre::Result<()> {
     let cli = Cli::parse();
 
     let result: eyre::Result<()> = try {
+        let ws_root_dir = get_root_dir(&RealSys).ok();
+        let trace_file_path = cli
+            .args
+            .file_trace_output
+            .clone()
+            .or_else(|| Some(PathBuf::from("./omni/trace/logs")));
+
+        let trace_file_path = if let Some(path) = trace_file_path {
+            Some(
+                if !path.has_root()
+                    && let Some(ref root) = ws_root_dir
+                {
+                    root.join(path)
+                } else {
+                    path
+                },
+            )
+        } else {
+            None
+        };
+
         let tracing_config = TracingConfig {
             stderr_trace_enabled: cli.args.stderr_trace,
-            file_path: cli
-                .args
-                .file_trace_output
-                .clone()
-                .or_else(|| Some(PathBuf::from("./omni/trace/logs"))),
+            file_path: trace_file_path,
             file_trace_level: cli.args.file_trace_level.value(),
             stdout_trace_level: cli.args.stdout_trace_level.value(),
         };
@@ -104,7 +130,13 @@ pub async fn main() -> eyre::Result<()> {
             trace::trace!("Tracing config: {:?}", tracing_config);
         }
 
-        run(&cli.subcommand, &cli.args, &tracing_config).await?
+        run(
+            &cli.subcommand,
+            &cli.args,
+            &tracing_config,
+            ws_root_dir.as_deref(),
+        )
+        .await?
     };
 
     if cfg!(debug_assertions) {
