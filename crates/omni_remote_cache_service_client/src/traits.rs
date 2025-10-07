a45a3a8191fedf8b6ca9a3397c6d1b99d4650ec2
Clarence Manuel <rencedm112@gmail.com>
2025-10-07 21:16:04 +0800

    feat(omni_remote_cache_service_client): add RemoteCacheServiceClient trait

---
+++ crates/omni_remote_cache_service_client/src/traits.rs
@@ -0,0 +1,72 @@
+use async_trait::async_trait;
+use bytes::Bytes;
+use derive_new::new;
+use strum::{EnumDiscriminants, IntoDiscriminant as _};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct RemoteAccessArgs<'a> {
+    pub api_key: &'a str,
+    pub tenant: &'a str,
+    pub org: &'a str,
+    pub ws: &'a str,
+    pub env: &'a str,
+}
+
+#[async_trait]
+pub trait RemoteCacheServiceClient: Send + Sync + 'static {
+    async fn get_artifact(
+        &self,
+        remote: &RemoteAccessArgs,
+        digest: &str,
+    ) -> Result<Option<Bytes>, RemoteCacheServiceClientError>;
+
+    async fn put_artifact(
+        &self,
+        remote: &RemoteAccessArgs,
+        digest: &str,
+        artifact: Bytes,
+    ) -> Result<(), RemoteCacheServiceClientError>;
+}
+
+#[derive(Debug, thiserror::Error, new)]
+#[error("RemoteCacheServiceClientError: {inner:?}")]
+pub struct RemoteCacheServiceClientError {
+    inner: RemoteCacheServiceClientErrorInner,
+    kind: RemoteCacheServiceClientErrorKind,
+}
+
+impl RemoteCacheServiceClientError {
+    pub fn custom<T: Into<eyre::Report>>(inner: T) -> Self {
+        let inner = inner.into();
+        Self {
+            inner: RemoteCacheServiceClientErrorInner::Custom(inner),
+            kind: RemoteCacheServiceClientErrorKind::Custom,
+        }
+    }
+}
+
+impl RemoteCacheServiceClientError {
+    #[allow(unused)]
+    pub fn kind(&self) -> RemoteCacheServiceClientErrorKind {
+        self.kind
+    }
+}
+
+impl<T: Into<RemoteCacheServiceClientErrorInner>> From<T>
+    for RemoteCacheServiceClientError
+{
+    fn from(inner: T) -> Self {
+        let inner = inner.into();
+        Self {
+            kind: inner.discriminant(),
+            inner,
+        }
+    }
+}
+
+#[derive(Debug, EnumDiscriminants, thiserror::Error, new)]
+#[strum_discriminants(vis(pub), name(RemoteCacheServiceClientErrorKind))]
+enum RemoteCacheServiceClientErrorInner {
+    #[error(transparent)]
+    Custom(#[from] eyre::Report),
+}
