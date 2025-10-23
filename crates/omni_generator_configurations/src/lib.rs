f80884b832a50cdf087ae1f8fd6a3ae25f3f1c18
Clarence Manuel <rencedm112@gmail.com>
2025-10-23 22:17:41 +0800

    feat: add initial omni_generator_configurations crate implementation

---
+++ crates/omni_generator_configurations/src/lib.rs
@@ -0,0 +1,7 @@
+mod action_configuration;
+mod generator_configuration;
+mod prompt_configuration;
+
+pub use action_configuration::*;
+pub use generator_configuration::*;
+pub use prompt_configuration::*;
