35ab78317456d27ef08c47bba9fa6ba087d036fc
Clarence Manuel <rencedm112@gmail.com>
2025-10-24 15:49:48 +0800

    feat: update error message on platform-helpers [skip ci]

--- packages/system-interface/src/platform-helpers/index.ts
+++ packages/system-interface/src/platform-helpers/index.ts
@@ -1,7 +1,7 @@
 export function throwIfBunNotAvailable(): void {
     if (typeof Bun === "undefined") {
         throw new Error(
-            "Bun is not available, please use other file system implementation",
+            "Bun is not available, please use other implementation",
         );
     }
 }
@@ -9,7 +9,7 @@ export function throwIfBunNotAvailable(): void {
 export function throwIfDenoNotAvailable(): void {
     if (typeof Deno === "undefined") {
         throw new Error(
-            "Deno is not available, please use other file system implementation",
+            "Deno is not available, please use other implementation",
         );
     }
 }
