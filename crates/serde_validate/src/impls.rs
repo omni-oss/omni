f366befad854bf32f4f4ed50e42a43cd23e320bc
Clarence Manuel <rencedm112@gmail.com>
2025-11-03 10:23:11 +0800

    feat(serde_validate): implement generic tuple implementations for validators

--- crates/serde_validate/src/impls.rs
+++ crates/serde_validate/src/impls.rs
@@ -1,4 +1,6 @@
-pub use crate::macro_helpers::impl_compound_static_validators;
+pub use crate::macro_helpers::{
+    impl_compound_static_validators, impl_compound_validators,
+};
 
 impl_compound_static_validators!(T => V1, V2);
 impl_compound_static_validators!(T => V1, V2, V3);
@@ -15,3 +17,19 @@ impl_compound_static_validators!(T => V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V
 impl_compound_static_validators!(T => V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13, V14);
 impl_compound_static_validators!(T => V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13, V14, V15);
 impl_compound_static_validators!(T => V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13, V14, V15, V16);
+
+impl_compound_validators!(T => V1, V2);
+impl_compound_validators!(T => V1, V2, V3);
+impl_compound_validators!(T => V1, V2, V3, V4);
+impl_compound_validators!(T => V1, V2, V3, V4, V5);
+impl_compound_validators!(T => V1, V2, V3, V4, V5, V6);
+impl_compound_validators!(T => V1, V2, V3, V4, V5, V6, V7);
+impl_compound_validators!(T => V1, V2, V3, V4, V5, V6, V7, V8);
+impl_compound_validators!(T => V1, V2, V3, V4, V5, V6, V7, V8, V9);
+impl_compound_validators!(T => V1, V2, V3, V4, V5, V6, V7, V8, V9, V10);
+impl_compound_validators!(T => V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11);
+impl_compound_validators!(T => V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12);
+impl_compound_validators!(T => V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13);
+impl_compound_validators!(T => V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13, V14);
+impl_compound_validators!(T => V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13, V14, V15);
+impl_compound_validators!(T => V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13, V14, V15, V16);
--- crates/serde_validate/src/macro_helpers.rs
+++ crates/serde_validate/src/macro_helpers.rs
@@ -8,3 +8,20 @@ pub macro impl_compound_static_validators($t:ident => $($v:ident),*$(,)?) {
         }
     }
 }
+
+pub macro impl_compound_validators($t:ident => $($v:ident),*$(,)?) {
+    impl <$t, $($v: $crate::Validator<$t>),*> $crate::Validator<$t> for ($($v),*) {
+
+        #[allow(non_snake_case)]
+        #[allow(nonstandard_style)]
+        fn validate(&self, value: &$t) -> Result<(), String> {
+            let ($($v,)*) = self;
+
+            $(
+                $v.validate(value)?;
+            )*
+
+            Ok(())
+        }
+    }
+}
