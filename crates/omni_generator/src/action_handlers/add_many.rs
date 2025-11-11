a75878b5f6e7e8cfe9b97d329496f9b079bc7d3f
Clarence Manuel <rencedm112@gmail.com>
2025-11-11 11:17:44 +0800

    refactor(omni_generator): cleanup add-inline, add, and add-many handlers

--- crates/omni_generator/src/action_handlers/add_many.rs
+++ crates/omni_generator/src/action_handlers/add_many.rs
@@ -1,17 +1,9 @@
-use std::path::Path;
-
+use super::add_commons::add_one;
 use omni_discovery::Discovery;
 use omni_generator_configurations::AddManyActionConfiguration;
 use tera::Tera;
 
-use crate::{
-    GeneratorSys,
-    action_handlers::{
-        HandlerContext,
-        utils::{ensure_dir_exists, get_output_path, overwrite},
-    },
-    error::{Error, ErrorInner},
-};
+use crate::{GeneratorSys, action_handlers::HandlerContext, error::Error};
 
 pub async fn add_many<'a>(
     config: &AddManyActionConfiguration,
@@ -41,48 +33,15 @@ pub async fn add_many<'a>(
             .strip_prefix(&ctx.generator_dir)
             .expect("should have value");
 
-        let output_path = get_output_path(
-            config.base.common.target.as_deref(),
-            stripped_path,
-            config.base_path.as_deref(),
+        add_one(
+            &template_file,
+            None,
+            |ctx| tera.render(&stripped_path.to_string_lossy(), ctx),
+            &config.base.common,
             ctx,
-            &["tpl"],
             sys,
         )
         .await?;
-        let expanded_output = tera::Tera::one_off(
-            &output_path.to_string_lossy(),
-            ctx.tera_context_values,
-            false,
-        )?;
-
-        let output_path = Path::new(&expanded_output);
-        if let Some(did_overwrite) = overwrite(
-            &output_path,
-            ctx.overwrite.or(config.base.common.overwrite),
-            sys,
-        )
-        .await?
-            && !did_overwrite
-        {
-            trace::info!("Skipped writing to path {}", output_path.display());
-            return Ok(());
-        }
-        ensure_dir_exists(
-            &output_path.parent().expect("should have parent"),
-            sys,
-        )
-        .await?;
-        let result = tera.render(
-            &stripped_path.to_string_lossy(),
-            ctx.tera_context_values,
-        )?;
-        sys.fs_write_async(&output_path, &result)
-            .await
-            .map_err(|e| {
-                ErrorInner::new_failed_to_write_file(&output_path, e)
-            })?;
-        trace::info!("Wrote to path {}", output_path.display());
     }
     Ok(())
 }
