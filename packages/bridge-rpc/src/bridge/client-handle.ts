8bd027f58e092cb6b14d758d36b54dfd36f888ca
Clarence Manuel <rencedm112@gmail.com>
2026-01-27 18:00:43 +0800

    chore(bridge-rpc): add ClientHandle

--- packages/bridge-rpc/src/bridge/client-handle.ts
+++ packages/bridge-rpc/src/bridge/client-handle.ts
@@ -1,1 +1,6 @@
-export class ClientHandle {}
+import type { Id } from "@/id";
+import type { PendingRequest } from "./client/request";
+
+export interface ClientHandle {
+    requestWithId(id: Id, path: string): Promise<PendingRequest>;
+}
