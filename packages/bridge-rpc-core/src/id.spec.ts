bb4790904c75b8cf10821019069124707d14c52d
Clarence Manuel <Clarence.Manuel@dayforce.com>
2026-05-27 21:15:29 +0800

    chore: update packages versions and configurations

--- packages/bridge-rpc-core/src/id.spec.ts
+++ packages/bridge-rpc-core/src/id.spec.ts
@@ -10,18 +10,14 @@ describe("Id", () => {
     // the javascript implementation is very slow
     // so we need to run this test for a long time to not timeout the test
     // also limited only to 50_000 iterations since the usual 1_000_000 test is slow on js
-    it(
-        "should never be equal to another Id",
-        {
-            timeout: 10_000,
-        },
-        () => {
-            const keys = new Set<bigint>();
-            for (let i = 0; i < 50_000; i++) {
-                const id = Id.create();
-                expect(keys.has(id.getValue())).toBe(false);
-                keys.add(id.getValue());
-            }
-        },
-    );
+    it("should never be equal to another Id", {
+        timeout: 10_000,
+    }, () => {
+        const keys = new Set<bigint>();
+        for (let i = 0; i < 50_000; i++) {
+            const id = Id.create();
+            expect(keys.has(id.getValue())).toBe(false);
+            keys.add(id.getValue());
+        }
+    });
 });
