e1a95c27e6339c8315457ce90cafe75e1eb55b7a
Clarence Manuel <Clarence.Manuel@dayforce.com>
2026-06-04 21:35:33 +0800

    chore(@omni-oss/omni-remote-cache-service-tests): utilize new test preset

--- tests/omni-remote-cache-service-tests/vite.config.ts
+++ tests/omni-remote-cache-service-tests/vite.config.ts
@@ -1,15 +1,17 @@
-import baseConfig from "@omni-oss/vite-config/library";
-import { mergeConfig, type UserConfig } from "vite";
+import { createConfig } from "@omni-oss/vite-config/test";
+import packageJson from "./package.json";
 
-export default mergeConfig(baseConfig, {
-    build: {
-        minify: "esbuild",
-        lib: {
-            entry: "src/index.ts",
-            formats: ["es", "cjs"],
-            fileName: (format) =>
-                `orcs-api-test.${format === "cjs" ? "cjs" : "mjs"}`,
-            name: "OrcsApiTest",
+export default createConfig({
+    package: packageJson,
+    overrides: {
+        build: {
+            lib: {
+                entry: "src/index.ts",
+                formats: ["es", "cjs"],
+                fileName: (format) =>
+                    `orcs-api-test.${format === "cjs" ? "cjs" : "mjs"}`,
+                name: "OrcsApiTest",
+            },
         },
     },
-} satisfies UserConfig);
+});
