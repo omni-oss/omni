bdf474064f29f6ffcc50e911b5c8c1c015fd416b
Clarence Manuel <Clarence.Manuel@dayforce.com>
2026-06-20 13:39:02 +0800

    fix(@omni-oss/gen-sdk-core): export additional types

--- packages/gen-sdk-core/src/index.ts
+++ packages/gen-sdk-core/src/index.ts
@@ -1,1 +1,3 @@
+export type { Logger } from "@omni-oss/log";
+export type { FileSystem, Process, System } from "@omni-oss/system-interface";
 export type { GeneratorScriptContext } from "./generator-script-context";
