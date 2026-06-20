3eecf9c7a71b02991780cefc35f898bde31bfdca
Clarence Manuel <Clarence.Manuel@dayforce.com>
2026-06-20 13:36:50 +0800

    fix(@omni-oss/vite-config): add additional configuration for library preset

--- packages/vite-config/src/library.ts
+++ packages/vite-config/src/library.ts
@@ -10,10 +10,15 @@ export default config;
 
 export type LibraryConfigOptions = BaseConfigOptions & {
     typesTsConfigPath?: string;
-    bundleTypes?: boolean;
+    bundleTypes?: Exclude<
+        Parameters<typeof dts>["0"],
+        undefined
+    >["bundleTypes"];
 };
 
 export function createConfig(options?: LibraryConfigOptions) {
+    const typesTsConfigPath =
+        options?.typesTsConfigPath || "./tsconfig.types.json";
     return baseCreateConfig({
         ...options,
         externalizeDeps: options?.externalizeDeps ?? true,
@@ -21,9 +26,21 @@ export function createConfig(options?: LibraryConfigOptions) {
             ...options?.overrides,
             plugins: [
                 dts({
-                    bundleTypes: options?.bundleTypes ?? false,
-                    tsconfigPath:
-                        options?.typesTsConfigPath || "./tsconfig.types.json",
+                    pathsToAliases: true,
+                    bundleTypes:
+                        typeof options?.bundleTypes === "boolean"
+                            ? options.bundleTypes
+                                ? {
+                                      extractorConfig: {
+                                          compiler: {
+                                              tsconfigFilePath:
+                                                  typesTsConfigPath,
+                                          },
+                                      },
+                                  }
+                                : false
+                            : (options?.bundleTypes ?? false),
+                    tsconfigPath: typesTsConfigPath,
                 }),
                 ...(options?.overrides?.plugins ?? []),
             ],
