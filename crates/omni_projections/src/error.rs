4ada17be39f67087b5724411603c06e0e3e2e414
Clarence Manuel <rencedm112@gmail.com>
2026-08-27 16:22:45 +0800

    chore(omni_projections): scaffold crate, error type, and dependencies

---
+++ crates/omni_projections/src/error.rs
@@ -0,0 +1,35 @@
+use strum::{EnumDiscriminants, IntoDiscriminant as _};
+
+#[derive(Debug, thiserror::Error)]
+#[error(transparent)]
+pub struct ProjectionError(pub(crate) ProjectionErrorInner);
+
+impl ProjectionError {
+    pub fn custom(message: impl Into<String>) -> Self {
+        Self(ProjectionErrorInner::Custom(eyre::Report::msg(
+            message.into(),
+        )))
+    }
+
+    pub fn kind(&self) -> ProjectionErrorKind {
+        self.0.discriminant()
+    }
+}
+
+impl<T: Into<ProjectionErrorInner>> From<T> for ProjectionError {
+    fn from(inner: T) -> Self {
+        Self(inner.into())
+    }
+}
+
+#[derive(Debug, thiserror::Error, EnumDiscriminants)]
+#[strum_discriminants(vis(pub), name(ProjectionErrorKind))]
+pub(crate) enum ProjectionErrorInner {
+    #[error(transparent)]
+    Custom(#[from] eyre::Report),
+
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+}
+
+pub type Result<T, E = ProjectionError> = std::result::Result<T, E>;
---
+++ crates/omni_projections/src/lib.rs
@@ -0,0 +1,5 @@
+#![allow(clippy::redundant_field_names)]
+
+mod error;
+
+pub use error::*;
