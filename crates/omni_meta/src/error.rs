40f367b0621dc568e61dd276b75482167edad4ec
Clarence Manuel <rencedm112@gmail.com>
2026-09-10 19:18:15 +0800

    chore: scaffold omni_meta crate

---
+++ crates/omni_meta/src/error.rs
@@ -0,0 +1,34 @@
+use derive_new::new;
+use strum::{EnumDiscriminants, IntoDiscriminant as _};
+
+#[derive(Debug, thiserror::Error, new)]
+#[error(transparent)]
+pub struct Error(pub(crate) ErrorInner);
+
+impl Error {
+    pub fn custom(message: impl Into<String>) -> Self {
+        Self(ErrorInner::Custom(eyre::Report::msg(message.into())))
+    }
+}
+
+impl Error {
+    #[allow(unused)]
+    pub fn kind(&self) -> ErrorKind {
+        self.0.discriminant()
+    }
+}
+
+impl<T: Into<ErrorInner>> From<T> for Error {
+    fn from(inner: T) -> Self {
+        let inner = inner.into();
+
+        Self(inner)
+    }
+}
+
+#[derive(Debug, thiserror::Error, EnumDiscriminants, new)]
+#[strum_discriminants(vis(pub), name(ErrorKind))]
+pub(crate) enum ErrorInner {
+    #[error(transparent)]
+    Custom(#[from] eyre::Report),
+}
