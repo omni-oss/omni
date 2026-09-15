0f2db19ce600f9b1cf86706ce048f374d8d59698
Clarence Manuel <rencedm112@gmail.com>
2026-09-16 04:59:49 +0800

    refactor(omni_remote_source): remove the rev-keyed fetch path superseded by the shared store

--- crates/omni_remote_source/src/manager/default_impl.rs
+++ crates/omni_remote_source/src/manager/default_impl.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::{HashMap, HashSet},
+    collections::HashSet,
     ffi::OsString,
     path::{Path, PathBuf},
     sync::atomic::{AtomicU64, Ordering},
@@ -9,7 +9,6 @@ use std::{
 use omni_git_utils::CloneInfo;
 use omni_lockfile::{Lockfile, data::LockfileData};
 use omni_utils::lock::LockGuard;
-use tokio::task::JoinSet;
 use url::Url;
 
 use crate::{
@@ -281,124 +280,6 @@ impl<TSys: RemoteSourceSys> RemoteSourceManager<TSys> {
 }
 
 impl<TSys: RemoteSourceSys> RemoteSourceManager<TSys> {
-    pub async fn pull_git_repo(
-        &self,
-        uri: &Url,
-        rev: &str,
-    ) -> Result<PathBuf, Error> {
-        let commit = self.lockfile.get_git_commit(uri, rev).await;
-        let dest = self.git_dest_dir(uri, Some(rev))?;
-
-        match commit {
-            Some(commit) => {
-                if !self.sys.fs_exists_no_err_async(&dest).await {
-                    log::trace!("created dir: {dest:?}");
-                    self.sys.fs_create_dir_all_async(&dest).await?;
-                    self.clone_repo_inner(&dest, uri, &commit).await?;
-                }
-            }
-            None => {
-                if self.sys.fs_exists_no_err_async(&dest).await {
-                    log::trace!("removing dir: {dest:?}");
-                    self.sys.fs_remove_dir_all_async(&dest).await?;
-                }
-                log::trace!("created dir: {dest:?}");
-                self.sys.fs_create_dir_all_async(&dest).await?;
-
-                let clone = self
-                    .clone_repo_inner(
-                        &dest,
-                        uri,
-                        commit.as_deref().unwrap_or(rev),
-                    )
-                    .await?;
-
-                self.lockfile
-                    .lock_git_commit(uri, rev, &clone.commit)
-                    .await?;
-            }
-        }
-
-        Ok(dest)
-    }
-
-    pub async fn retain_git_sources(
-        &self,
-        git_sources: &[(&Url, &str)],
-    ) -> Result<(), Error> {
-        let mut source_map = HashMap::<&Url, HashSet<&str>>::new();
-
-        for (url, rev) in git_sources {
-            if let Some(revs) = source_map.get_mut(url) {
-                revs.insert(*rev);
-            } else {
-                let set = HashSet::from_iter([*rev]);
-
-                source_map.insert(*url, set);
-            }
-        }
-
-        let mut rm_dirs: Vec<(Url, Option<String>)> = vec![];
-
-        self.lockfile
-            .modify(|d| {
-                match d {
-                    LockfileData::V1_0_0(v1) => {
-                        v1.git.retain(|k, v| {
-                            if !source_map.contains_key(k) {
-                                rm_dirs.push((k.clone(), None));
-                                return false;
-                            }
-
-                            let revs =
-                                source_map.get(k).expect("should have value");
-
-                            v.retain(|x, _| {
-                                let should_retain = revs.contains(x.as_str());
-
-                                if !should_retain {
-                                    rm_dirs.push((k.clone(), Some(x.clone())));
-                                }
-
-                                should_retain
-                            });
-
-                            if v.is_empty() {
-                                rm_dirs.push((k.clone(), None));
-                                return false;
-                            }
-
-                            true
-                        });
-                    }
-                }
-
-                Ok(())
-            })
-            .await?;
-
-        let mut rm_tasks = JoinSet::new();
-        for (uri, rev) in rm_dirs {
-            let dest = self.git_dest_dir(&uri, rev.as_deref())?;
-            let sys = self.sys.clone();
-
-            rm_tasks.spawn(async move {
-                if sys.fs_exists_async(&dest).await? {
-                    sys.fs_remove_dir_all_async(&dest).await?;
-                    log::debug!("removed stale git source directory: {dest:?}");
-                }
-
-                Ok::<_, Error>(())
-            });
-        }
-
-        for t in rm_tasks.join_all().await {
-            t?;
-        }
-
-        Ok(())
-    }
-
     pub async fn lock(&self) -> Result<(), Error> {
         self.lockfile.save(&self.sys).await?;
         Ok(())
@@ -453,21 +334,6 @@ impl<TSys: RemoteSourceSys> RemoteSourceManager<TSys> {
         .await?;
         log::trace!("cloned git repo uri: {}, rev: {}", uri, commit,);
         Ok(clone)
-    }
-
-    fn git_dest_dir(
-        &self,
-        uri: &Url,
-        rev: Option<&str>,
-    ) -> Result<PathBuf, omni_git_utils::Error> {
-        let slug = omni_git_utils::url_to_safe_dir_name(uri.as_str())?;
-        let path = self.store_root_path.join("git").join(slug);
-
-        Ok(if let Some(rev) = rev {
-            path.join(rev)
-        } else {
-            path
-        })
     }
 
     fn git_commit_dir(
